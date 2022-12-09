8973b2bd5d66cedc4608fc8fcecd594fb99d9480
Benisa <120224734+Benisa29@users.noreply.github.com>
2022-12-10 04:24:28 +0600

    dz 2

---
+++ DZ2_Benisa_21_01/main.js
@@ -0,0 +1,22 @@
+// 1
+var arr = [1,2,4,5,6,7,8,9]
+console.log(arr)
+arr.push(10)
+console.log(arr)
+// 3
+// var colors = prompt('Choice color! (red, yellow, green)').toLowerCase()
+//
+// switch (colors) {
+//     case 'red':
+//         alert('STOP!')
+//         break
+//     case 'yellow':
+//         alert('Wait!')
+//         break
+//     case 'green':
+//         alert('GO')
+//         break
+//     default:
+//         alert('Choice color! (red, yellow, green)')
+//         break
+// }
